88ad045795ba18e0e18fbac92fbd477080d58ef1
Marcus Alsterfjord <marcus.alsterfjord@inyett.se>
2017-11-28 15:35:43 +0100

    Add missing function

--- chartjs-plugin-trendline.js
+++ chartjs-plugin-trendline.js
@@ -39,3 +39,30 @@ var pluginTrendlineLinear = {
 	}
 };
 Chart.plugins.register(pluginTrendlineLinear);
+
+function LineFitter()
+{
+	this.count = 0;
+	this.sumX = 0;
+	this.sumX2 = 0;
+	this.sumXY = 0;
+	this.sumY = 0;
+}
+
+LineFitter.prototype = {
+	'add': function(x, y)
+	{
+		this.count++;
+		this.sumX += x;
+		this.sumX2 += x*x;
+		this.sumXY += x*y;
+		this.sumY += y;
+	},
+	'project': function(x)
+	{
+		var det = this.count * this.sumX2 - this.sumX * this.sumX;
+		var offset = (this.sumX2 * this.sumY - this.sumX * this.sumXY) / det;
+		var scale = (this.count * this.sumXY - this.sumX * this.sumY) / det;
+		return offset + x * scale;
+	}
+};
